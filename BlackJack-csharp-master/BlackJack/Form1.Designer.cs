4264b778a597c327fd5f60c9fd2c9c3eac0fdca3
Riquelme29 <71471114+Riquelme29@users.noreply.github.com>
2021-05-09 21:58:08 -0300

    Add files via upload

---
+++ BlackJack-csharp-master/BlackJack/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿
+using System;
+
+namespace BlackJack
+{
+    partial class Form_jogo
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_jogo));
+            this.btn_jogar_1 = new System.Windows.Forms.Button();
+            this.btn_reiniciar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbl_Pontos_A = new System.Windows.Forms.Label();
+            this.lbl_legenda = new System.Windows.Forms.Label();
+            this.btn_parar_1 = new System.Windows.Forms.Button();
+            this.btn_parar_2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btn_jogar_2 = new System.Windows.Forms.Button();
+            this.lbl_Pontos_B = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_Resultado = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_jogar_1
+            // 
+            this.btn_jogar_1.BackColor = System.Drawing.Color.DarkRed;
+            this.btn_jogar_1.Font = new System.Drawing.Font("Showcard Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_jogar_1.Location = new System.Drawing.Point(26, 452);
+            this.btn_jogar_1.Name = "btn_jogar_1";
+            this.btn_jogar_1.Size = new System.Drawing.Size(110, 29);
+            this.btn_jogar_1.TabIndex = 0;
+            this.btn_jogar_1.Text = "Jogar";
+            this.btn_jogar_1.UseVisualStyleBackColor = false;
+            this.btn_jogar_1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_reiniciar
+            // 
+            this.btn_reiniciar.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_reiniciar.Font = new System.Drawing.Font("Algerian", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_reiniciar.ForeColor = System.Drawing.Color.DarkGreen;
+            this.btn_reiniciar.Location = new System.Drawing.Point(295, 12);
+            this.btn_reiniciar.Name = "btn_reiniciar";
+            this.btn_reiniciar.Size = new System.Drawing.Size(206, 55);
+            this.btn_reiniciar.TabIndex = 1;
+            this.btn_reiniciar.Text = "RESTART";
+            this.btn_reiniciar.UseVisualStyleBackColor = false;
+            this.btn_reiniciar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(26, 83);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(246, 353);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbl_Pontos_A
+            // 
+            this.lbl_Pontos_A.AutoSize = true;
+            this.lbl_Pontos_A.Font = new System.Drawing.Font("Algerian", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_Pontos_A.Location = new System.Drawing.Point(333, 196);
+            this.lbl_Pontos_A.Name = "lbl_Pontos_A";
+            this.lbl_Pontos_A.Size = new System.Drawing.Size(30, 30);
+            this.lbl_Pontos_A.TabIndex = 3;
+            this.lbl_Pontos_A.Text = "0";
+            // 
+            // lbl_legenda
+            // 
+            this.lbl_legenda.AutoSize = true;
+            this.lbl_legenda.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_legenda.Font = new System.Drawing.Font("Algerian", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_legenda.ForeColor = System.Drawing.Color.White;
+            this.lbl_legenda.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lbl_legenda.Location = new System.Drawing.Point(333, 142);
+            this.lbl_legenda.Name = "lbl_legenda";
+            this.lbl_legenda.Size = new System.Drawing.Size(132, 32);
+            this.lbl_legenda.TabIndex = 4;
+            this.lbl_legenda.Text = "Pontos:";
+            // 
+            // btn_parar_1
+            // 
+            this.btn_parar_1.BackColor = System.Drawing.Color.DarkRed;
+            this.btn_parar_1.Font = new System.Drawing.Font("Showcard Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_parar_1.Location = new System.Drawing.Point(162, 452);
+            this.btn_parar_1.Name = "btn_parar_1";
+            this.btn_parar_1.Size = new System.Drawing.Size(110, 29);
+            this.btn_parar_1.TabIndex = 5;
+            this.btn_parar_1.Text = "Parar";
+            this.btn_parar_1.UseVisualStyleBackColor = false;
+            this.btn_parar_1.Click += new System.EventHandler(this.btn_parar_1_Click);
+            // 
+            // btn_parar_2
+            // 
+            this.btn_parar_2.BackColor = System.Drawing.Color.DarkBlue;
+            this.btn_parar_2.Font = new System.Drawing.Font("Showcard Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_parar_2.Location = new System.Drawing.Point(656, 452);
+            this.btn_parar_2.Name = "btn_parar_2";
+            this.btn_parar_2.Size = new System.Drawing.Size(110, 29);
+            this.btn_parar_2.TabIndex = 8;
+            this.btn_parar_2.Text = "Parar";
+            this.btn_parar_2.UseVisualStyleBackColor = false;
+            this.btn_parar_2.Click += new System.EventHandler(this.btn_parar_2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(520, 83);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(246, 353);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btn_jogar_2
+            // 
+            this.btn_jogar_2.BackColor = System.Drawing.Color.DarkBlue;
+            this.btn_jogar_2.Font = new System.Drawing.Font("Showcard Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_jogar_2.Location = new System.Drawing.Point(520, 452);
+            this.btn_jogar_2.Name = "btn_jogar_2";
+            this.btn_jogar_2.Size = new System.Drawing.Size(110, 29);
+            this.btn_jogar_2.TabIndex = 6;
+            this.btn_jogar_2.Text = "Jogar";
+            this.btn_jogar_2.UseVisualStyleBackColor = false;
+            this.btn_jogar_2.Click += new System.EventHandler(this.btn_jogar_2_Click);
+            // 
+            // lbl_Pontos_B
+            // 
+            this.lbl_Pontos_B.AutoSize = true;
+            this.lbl_Pontos_B.Font = new System.Drawing.Font("Algerian", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_Pontos_B.Location = new System.Drawing.Point(435, 196);
+            this.lbl_Pontos_B.Name = "lbl_Pontos_B";
+            this.lbl_Pontos_B.Size = new System.Drawing.Size(30, 30);
+            this.lbl_Pontos_B.TabIndex = 9;
+            this.lbl_Pontos_B.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(393, 208);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 15);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "x";
+            // 
+            // lbl_Resultado
+            // 
+            this.lbl_Resultado.AutoSize = true;
+            this.lbl_Resultado.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_Resultado.Location = new System.Drawing.Point(278, 251);
+            this.lbl_Resultado.Name = "lbl_Resultado";
+            this.lbl_Resultado.Size = new System.Drawing.Size(0, 21);
+            this.lbl_Resultado.TabIndex = 11;
+            // 
+            // Form_jogo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(782, 502);
+            this.Controls.Add(this.lbl_Resultado);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_Pontos_B);
+            this.Controls.Add(this.btn_parar_2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btn_jogar_2);
+            this.Controls.Add(this.btn_parar_1);
+            this.Controls.Add(this.lbl_legenda);
+            this.Controls.Add(this.lbl_Pontos_A);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_reiniciar);
+            this.Controls.Add(this.btn_jogar_1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form_jogo";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "Black Jack (Versão1.0)";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_jogar_1;
+        private System.Windows.Forms.Button btn_reiniciar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbl_Pontos_A;
+        private System.Windows.Forms.Label lbl_legenda;
+        private System.Windows.Forms.Button btn_parar_1;
+        private System.Windows.Forms.Button btn_parar_2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btn_jogar_2;
+        private System.Windows.Forms.Label lbl_Pontos_B;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_Resultado;
+
+        public EventHandler Form_jogo_Load { get; private set; }
+    }
+}
+
